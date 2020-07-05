31b572e3251c3d1b73a15c6ba161b484ab5a2e4a
liule-peak <839119912@qq.com>
2020-07-05 21:42:16 +0800

    angree

--- new.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	cout << "this is a new git file" << endl;
-	return 0;
-}
--- test.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	cout << "this is a new test cpp file" << endl;
-	return 0;
-}
